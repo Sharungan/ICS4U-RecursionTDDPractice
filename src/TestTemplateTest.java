547f4b33a00ec05474045b776153f03866c20fbd
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-02 01:10:03 -0500

    Create TestTemplateTest.java

---
+++ src/TestTemplateTest.java
@@ -0,0 +1,22 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import yourClassToTest;
+
+/**
+* See https://github.com/mrfabroa/Fabroa-ICS4U-NotesExamples/blob/main/src/tdd/TDDExampleTest.java for an example
+*/
+public class TestTemplateTest {
+
+    /**
+     * An initial test
+     */
+    @Test
+    public void Test1(){
+        // make assertion statement(s)
+        //assertEquals(EXPECTEDVALUE, yourClassToTest.methodToTest());
+    }
+    
+    // add more tests
+}
